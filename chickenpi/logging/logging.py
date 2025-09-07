771bfefa31a18927773bb109d9c61fcc9ca0fafb
Jörg <joerg.sesterhenn@gmail.com>
2025-09-07 20:52:57 +0200

    fix: logging config

--- chickenpi/logging/logging.py
+++ chickenpi/logging/logging.py
@@ -1,5 +1,3 @@
 import logging.config
 
-logging.config.fileConfig(
-    "/home/joergsesterhenn/pi-controll/logger.ini", disable_existing_loggers=False
-)
+logging.config.fileConfig("logger.ini", disable_existing_loggers=False)
